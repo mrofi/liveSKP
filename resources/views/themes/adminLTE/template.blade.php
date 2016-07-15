d7f994746ab46f746bfe423d711d6b0af6f6d108
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2016-07-15 14:22:52 +0700

    remove search bar

--- resources/views/themes/adminLTE/template.blade.php
+++ resources/views/themes/adminLTE/template.blade.php
@@ -126,11 +126,11 @@ desired effect
               </ul>
             </li> -->
           </ul>
-          <form class="navbar-form navbar-left hidden-xs" role="search">
+          <!-- <form class="navbar-form navbar-left hidden-xs" role="search">
             <div class="form-group">
               <input type="text" class="form-control" id="navbar-search-input" placeholder="Search">
             </div>
-          </form>
+          </form> -->
         </div>
         <!-- Navbar Right Menu -->
         <div class="navbar-custom-menu">
